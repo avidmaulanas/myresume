c55b67ec722bf3eb373a2ead43a2bf20bdfb2ccf
Avid Maulana Saputra <avidmaulanas@gmail.com>
2016-03-05 12:51:35 +0700

    Disble google analytic and add Rack::Deflater

--- app/mailers/greeting_mailer.rb
+++ app/mailers/greeting_mailer.rb
@@ -5,6 +5,6 @@ class GreetingMailer < ApplicationMailer
     @email   = params[:email]
     @phone   = params[:phone]
     @message = params[:message]
-    mail(to: 'avid@41studio.com', subject: "Greeting from #{@name}")
+    mail(to: 'avidmaulanas@gmail.com', subject: "Greeting from #{@name}")
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,6 @@ module Myresume
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.middleware.use Rack::Deflater
   end
 end
