803e333969090b3f7c3484d9c8e36f2634df1fbc
Avid Maulana Saputra <avidmaulanas@gmail.com>
2016-05-18 19:52:42 +0700

    Add send email after submit greeting

--- app/controllers/greeting_controller.rb
+++ app/controllers/greeting_controller.rb
@@ -5,6 +5,7 @@ class GreetingController < ApplicationController
   def do_send
   	contact = Message.new(greeting_params)
   	if contact.save
+      GreetingMailer.send_email(greeting_params)
 	  	render json: { success: true, message: "OK" }
 	  else
 	  	render json: { success: false, message: "#{contact.errors.full_messages.join(', ')}. Please try again." }
