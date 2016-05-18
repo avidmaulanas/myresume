ad78e8734ebca740ea8064b8472f2953ff519e2c
Avid Maulana Saputra <avidmaulanas@gmail.com>
2016-05-18 20:24:32 +0700

    Fix sending email

--- app/controllers/greeting_controller.rb
+++ app/controllers/greeting_controller.rb
@@ -5,7 +5,7 @@ class GreetingController < ApplicationController
   def do_send
   	contact = Message.new(greeting_params)
   	if contact.save
-      GreetingMailer.send_email(greeting_params)
+      GreetingMailer.send_email(greeting_params).deliver_later
 	  	render json: { success: true, message: "OK" }
 	  else
 	  	render json: { success: false, message: "#{contact.errors.full_messages.join(', ')}. Please try again." }
